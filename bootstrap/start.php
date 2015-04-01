4b79763e3de424781c8eafce4c875476bf11b1ae
tbapi <portal@tbapi.com>
2015-04-01 21:21:32 +0800

    创建环境配置

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+    'ideapad'=> array('PC-20150125WKDA'),
 
 ));
 
