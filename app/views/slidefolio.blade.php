78c386bda2ca0a1ff431d9deb920383000096101
visionshi <vision.shi@yunzhihui.com>
2015-04-02 20:14:50 +0800

    首页文字

--- app/views/slidefolio.blade.php
+++ app/views/slidefolio.blade.php
@@ -48,11 +48,11 @@
             </div>
             <div class="collapse navbar-collapse" id="mobilemenu">
                 <ul class="nav navbar-nav navbar-right text-center">
-                    <li><a href="#top"><i class="service-icon fa fa-home"></i>&nbsp;Home</a></li>
-                    <li><a href="#about"><i class="service-icon fa fa-info"></i>&nbsp;About</a></li>
-                    <li><a href="#services"><i class="service-icon fa fa-laptop"></i>&nbsp;Services</a></li>
-                    <li><a href="#portfolio"><i class="service-icon fa fa-camera"></i>&nbsp;Portfolio</a></li>
-                    <li><a href="#contact"><i class="service-icon fa fa-envelope"></i>&nbsp;Contact</a></li>
+                    <li><a href="#top"><i class="service-icon fa fa-home"></i>&nbsp;首页</a></li>
+                    <li><a href="#about"><i class="service-icon fa fa-info"></i>&nbsp;关于</a></li>
+                    <li><a href="#services"><i class="service-icon fa fa-laptop"></i>&nbsp;服务</a></li>
+                    <li><a href="#portfolio"><i class="service-icon fa fa-camera"></i>&nbsp;鹦鹉展示</a></li>
+                    <li><a href="#contact"><i class="service-icon fa fa-envelope"></i>&nbsp;联系我们</a></li>
                 </ul>
             </div>
         </div>
